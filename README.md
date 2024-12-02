c30811a7bc211ff0b2fee575dbb246f58e79cbcf
Zikunz <yingzhengen01@gmail.com>
2024-12-01 20:31:12 -0800

    Cited dataset

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CMPT742-Visual Computing Lab I - Group Project
+## Dataset: https://facundoq.github.io/datasets/lsa64/
--- source/test.py
+++ source/test.py
@@ -10,10 +10,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--test', action='store_true')
 args = parser.parse_args()
 # data_dir = r"C:\LabAssignment\Final\data"
-data_dir = "" #update this if move "skeleton_data" folder into other folder
+data_dir = ""  # update this if move "skeleton_data" folder into other folder
 full_dataset = HandSkeleton(data_dir)
 
-train_indices, val_indices = train_test_split(range(len(full_dataset)), test_size=0.2, random_state=22)
+train_indices, val_indices = train_test_split(
+    range(len(full_dataset)), test_size=0.2, random_state=22)
 train_dataset = Subset(full_dataset, train_indices)
 val_dataset = Subset(full_dataset, val_indices)
 
@@ -26,24 +27,26 @@ learning_rate = 0.0001
 batch_size = 32
 num_epochs = 70
 device = 'cuda'
-model = CNNLSTMModel(num_classes=num_classes, 
-                    output_size=output_size, 
-                    lstm_hidden_size=lstm_hidden_size, 
-                    lstm_num_layers=lstm_num_layers, 
-                    dropout=dropout)
+model = CNNLSTMModel(num_classes=num_classes,
+                     output_size=output_size,
+                     lstm_hidden_size=lstm_hidden_size,
+                     lstm_num_layers=lstm_num_layers,
+                     dropout=dropout)
 model = model.to(device)
 
 # Define loss and optimizer
 criterion = nn.CrossEntropyLoss()
-    
-
-#Test
-#create skeleton
 
 
-utils.getSkeleton("042_test.mp4") #replace the video name to get skeleton of the video
+# Test
+# create skeleton
+
+
+# replace the video name to get skeleton of the video
+utils.getSkeleton("003_test.mp4")
 test_dataset = HandSkeleton("", train=False)
-test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=0)
+test_loader = DataLoader(test_dataset, batch_size=1,
+                         shuffle=False, num_workers=0)
 model.load_state_dict(torch.load("hand_skeleton_model_xiaohe_1.pth"))
 model.eval()
 
@@ -51,7 +54,8 @@ for face_point, hand_point, labels in test_loader:
     # print(f'face_point: {face_point}')
     # print(f'hand_point: {hand_point}')
     print(f'GT_labels: {labels}')
-    face_point, hand_point, labels = face_point.to(device), hand_point.to(device), labels.to(device)
+    face_point, hand_point, labels = face_point.to(
+        device), hand_point.to(device), labels.to(device)
     outputs = model(face_point, hand_point)
     # loss = criterion(outputs, labels)
 
@@ -59,6 +63,6 @@ for face_point, hand_point, labels in test_loader:
     # print(pred_output)
     _, predicted = torch.max(outputs, 1)
     print(f'pred_labels: {predicted[0]}')
-    
+
 
 print("End test")
